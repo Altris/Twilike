0c40da864e9dcfe04816818690580d3ea1d35036
Golling Jeremy <golling@codd.u-strasbg.fr>
2009-11-26 17:17:37 +0100

    Mappage des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   # The priority is based upon order of creation: first created -> highest priority.
-
+  map.ressource :user_session
+  map.root :controller =>"user_sessions", :action => "new"
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
