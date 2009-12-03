bb652bb820fb3512877552247339aeeeb68fdc46
Golling Jeremy <golling@codd.u-strasbg.fr>
2009-12-03 14:30:55 +0100

    Ajout de routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ ActionController::Routing::Routes.draw do |map|
   #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
 
+    map.resource :account, :controller => "users"
+    map.resources :users
+
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   map.resources :products
 
